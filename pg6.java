b511df9b7fd4625f9a8ac3f1e57cfad4ba578670
Srinithi.S <srinithi.s2022ai-ml@sece.ac.in>
2025-12-09 09:11:04 +0530

    adding pg6.java

---
+++ pg6.java
@@ -0,0 +1,16 @@
+//pattern matcher
+import java.io.*;
+import java.utils.*;
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc =new Scanner(System.in);
+		String inp =sc.nextLine();
+		System.out.println(UserMainCode.matchPattern)
+	}
+}
+class UserMainCode{
+	pubic static boolean matchPattern(String inp){
+		return inp.matchPattern("CPT-\\d(6)");
+	}
+}
