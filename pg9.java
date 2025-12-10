8c3f810ae3f4c5b44ab6e3313b1bb97b410aab2e
Srinithi.S <srinithi.s2022ai-ml@sece.ac.in>
2025-12-10 10:29:08 +0530

    ADDING pg9.java

---
+++ pg9.java
@@ -0,0 +1,19 @@
+//Regular Expression 2
+import java.util.*;
+class Main{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		String inp = sc.nextLine();
+		System.out.println(UserMainCode.regularExpression(inp));
+	}
+class UserMainCode{
+	public static boolean regularExpression(String inp){
+		// pattern p= Pattern.conpile("[a-zA-Z0-9][R][0-9][a-zA-Z0-9]");
+		// Matches m = p.matcher(inp);
+		// return m.matches();
+		return inp.matches("(^[2][1-9]$)|(^[3][0-9]$)|(^[4][0-5]$)");
+		
+	}
+}	
+	
+}
